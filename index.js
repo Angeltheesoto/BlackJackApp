7fac47b6a023e3c983b5842a382e8560b02405e0
Your Name01~ <you@example.com>
2022-04-12 14:27:27 -0400

    Finished practice section and commented it out

---
+++ index.js
@@ -0,0 +1,77 @@
+
+// let firstName = 'Angel';
+// let lastName = 'Soto';
+
+// let fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+
+ //---------------------------------
+// let name = 'Linda';
+// let greeting = 'Hi there';
+
+
+// function greet() {
+//  console.log(greeting + ", " + name + "!")
+// };
+
+// greet();
+ // ---------------------------
+
+// let myPoints = 3;
+
+// function add3Points() {
+//  myPoints += 3
+// };
+
+// function remove1Point() {
+//  myPoints -= 1
+// };
+
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+
+// console.log(myPoints);
+
+ // ----------------------------------
+
+ // let error = document.getElementById('error');
+
+ // function problem() {
+ //  console.log("button clicked")
+ //  error.textContent = "Something went wrong, please try again."
+ // }
+
+ //-------------------------------------
+
+// practice calculator------------------------------------------------
+ // let num1 = 100;
+ // let num2 = 100;
+ // document.getElementById("num1-el").textContent = num1;
+ // document.getElementById("num2-el").textContent = num2;
+ // let sum = document.getElementById('sum-el')
+
+ // function add() {
+ //  sum.textContent = 'Sum: ' + (num1 + num2)
+ // };
+
+ // function subtract() {
+ //  sum.textContent = 'Sum: ' + (num1 - num2)
+ // };
+
+ // function divide() {
+ //  sum.textContent = 'Sum: ' + (num1 / num2)
+ // };
+
+ // function multiply() {
+ //  sum.textContent = 'Sum: ' + (num1 * num2)
+ // };
+
+ // function reset() {
+ //  sum.textContent = 'Sum: ' 
+ // };
+ 

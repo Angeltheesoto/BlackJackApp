c754ab893f0a6ab10848f5b39bb5e1f771432a51
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-04-12 12:01:56 -0400

    Added started files.

---
+++ BlackJack_App/index.js
@@ -0,0 +1,49 @@
+
+// let firstName = 'Angel';
+// let lastName = 'Soto';
+
+// let fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+
+ //---------------------------------
+// let name = 'Linda';
+// let greeting = 'Hi there';
+
+
+// function greet() {
+//  console.log(greeting + ", " + name + "!")
+// };
+
+// greet();
+ // ---------------------------
+
+// let myPoints = 3;
+
+// function add3Points() {
+//  myPoints += 3
+// };
+
+// function remove1Point() {
+//  myPoints -= 1
+// };
+
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+
+// console.log(myPoints);
+
+ // ----------------------------------
+
+ // let error = document.getElementById('error');
+
+ // function problem() {
+ //  console.log("button clicked")
+ //  error.textContent = "Something went wrong, please try again."
+ // }
+
+ //-------------------------------------
